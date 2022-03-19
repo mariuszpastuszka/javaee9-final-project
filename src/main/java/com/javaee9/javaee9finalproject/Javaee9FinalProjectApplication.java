f38b7a92bff7944505cd6703ee801187c106ed39
AigarJ <aigar134@gmail.com>
2022-03-19 11:50:08 +0200

    [change] - temp. disabled security

--- src/main/java/com/javaee9/javaee9finalproject/Javaee9FinalProjectApplication.java
+++ src/main/java/com/javaee9/javaee9finalproject/Javaee9FinalProjectApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Javaee9FinalProjectApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Javaee9FinalProjectApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(Javaee9FinalProjectApplication.class, args);
+	}
 
 }
--- src/test/java/com/javaee9/javaee9finalproject/Javaee9FinalProjectApplicationTests.java
+++ src/test/java/com/javaee9/javaee9finalproject/Javaee9FinalProjectApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class Javaee9FinalProjectApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
