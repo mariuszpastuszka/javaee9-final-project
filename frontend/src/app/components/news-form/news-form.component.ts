7c2445272b1e9d30dac916a14eb66bc715124c39
maniek <mpastuszka@gmail.com>
2022-04-10 09:16:43 +0200

    [change] - post form in progress

--- frontend/src/app/components/news-form/news-form.component.ts
+++ frontend/src/app/components/news-form/news-form.component.ts
@@ -1,17 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-news-form',
   templateUrl: './news-form.component.html',
   styleUrls: ['./news-form.component.css']
 })
-export class NewsFormComponent implements OnInit {
+export class NewsFormComponent {
 
-  constructor() { }
+  postForm = this.formBuilder.group({
+    header: ['', Validators.required],
+    content: ['', Validators.required],
+    author: ['', Validators.required]
+  })
+  constructor(private formBuilder: FormBuilder) { }
 
-  ngOnInit(): void {
+  createPost() {
+    console.log("trying to create post....")
   }
-
 }
 // TODO:
 // 1). Service - for sending new news to backend
