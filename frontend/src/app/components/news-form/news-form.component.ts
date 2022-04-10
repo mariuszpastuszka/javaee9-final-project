116c6e4f43549e027a51c50d21760c0a94bfb67e
maniek <mpastuszka@gmail.com>
2022-04-10 11:15:50 +0200

    [change] - almost working form with nasty NullPointerException on server side

--- frontend/src/app/components/news-form/news-form.component.ts
+++ frontend/src/app/components/news-form/news-form.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { NewsFeedService } from '../../services/news-feed.service';
+import { Post } from '../../models/post';
 
 @Component({
   selector: 'app-news-form',
@@ -19,5 +20,17 @@ export class NewsFormComponent {
 
   onFormSubmit() {
     console.log("on submitting whole form")
+    console.log(`value from form: [${JSON.stringify(this.postForm.value)}]`)
+
+    let post: Post = {
+      id:       null,
+      author:   this.postForm.value.author,
+      content:  this.postForm.value.content,
+      header:   this.postForm.value.header,
+      creationTimestamp: null,
+      updateTimestamp: null
+    }
+
+    this.newsService.createNewPost(post);
   }
 }
